cde4eb1730a2302fab186bea20ba54aa5fdef32c
Mehmood Amjad <mehmood9501@gmail.com>
2023-04-05 00:21:47 +0500

    Added Parser

--- Distributed Parser/consumer.py
+++ Distributed Parser/consumer.py
@@ -18,8 +18,20 @@ class Consumer():
         )
     # Function to display the data received by the consumer
     def Receive(self):
-        for msg in self.consumer:
-            os.system("sudo docker exec sparktest-spark-master-1 spark-submit --master spark://172.18.0.2:7077 parser.py")
+        for message in consumer:
+            # Get the message value (which is the file content)
+            file_content = message.value
+
+            # Parse the CSV data
+            rows = file_content.decode('utf-8').split('\n')
+            data = [row.split(',') for row in rows]
+
+            # Save the CSV data to a local file
+            with open('received_file.csv', 'w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerows(data)
+        # for msg in self.consumer:
+        #     os.system("sudo docker exec sparktest-spark-master-1 spark-submit --master spark://172.18.0.2:7077 parser.py")
 # Main function which creates the consumer and calls the receive function
 def main():
     cons_obj = Consumer()
